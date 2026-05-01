afe1f40ab7e62fe067277dcddb36987cd07c3e39
Adrian <adrian.ligiewicz@gmail.com>
2026-05-01 19:55:28 +0200

    V0.1 - Initial commit

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update && upgrade -y
+sudo apt install -y nginx
+
+mkdir /opt/app
+mv ./index.html /opt/app/index.html
+
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'ssh'
+sudo ufw enable
+
+sudo systemctl start nginx
